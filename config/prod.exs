e5b7c165c54b764e3647f57dc1fdba2b80906b85
EssenceOfChaos <fjschiller@gmail.com>
2018-08-13 01:06:12 -0400

    update uri

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :easy, EasyWeb.Endpoint,
   load_from_system_env: true,
-  url: [scheme: "https", host: "https://easy-.herokuapp.com/", port: 443],
+  url: [scheme: "https", host: "https://ezcrm.herokuapp.com/", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json",
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
